adb4c1d1315692170e8208ad98a6cb42922620a2
Matheus Johann Araújo <47996428+matheusjohannaraujo@users.noreply.github.com>
2019-03-06 00:56:19 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,3 @@
-﻿/*
-    Faculdade: IPESU/FAREC
-    Curso: Bacharelado em Ciência da Computação
-    Disciplina: Linguagem de Programação Estruturada
-    Professor: Rômulo Lima
-    Período/Turno: 2º/Noite
-    Estudante: Matheus Johann Araújo
-*/
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
